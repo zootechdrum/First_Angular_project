fb309902879c7aa3ac06ab877f615c09bfb24333
Cesar Gomez <zootechdrum@gmail.com>
2019-04-21 23:37:56 -0700

    added 2 way data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName= "";
+  serverName =  '';
 
   constructor() {
     setTimeout(() => {
@@ -21,7 +21,7 @@ export class ServersComponent implements OnInit {
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
   }
-  onUpdateServerName(event: any) {
-    console.log(event);
-  }
+  // onUpdateServerName(event: any) {
+  //   this.serverName = event.target.value;
+  // }
 }
