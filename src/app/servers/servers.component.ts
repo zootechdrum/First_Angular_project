871ead04c9e1549e11e690cc23c0103321a73097
Cesar Gomez <zootechdrum@gmail.com>
2019-04-23 23:27:07 -0700

    added ngclass to server component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,18 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName =  'TestServerD';
+  serverName = 'TestServerD';
   serverCreated = false;
+  servers = ['Testserver", "Testserver 2'];
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
-   }
-
-  ngOnInit() {
   }
+
+  ngOnInit() {}
   onCreateServer() {
+    this.servers.push(this.serverName);
     this.serverCreated = true;
     this.serverCreationStatus = 'Server was created ' + this.serverName;
   }
