2647624305f46f3c4830f3fcdf18ff09dff901a4
Cesar Gomez <zootechdrum@gmail.com>
2019-05-01 22:31:38 -0700

    broke down recip-list to smaller component

--- src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipes-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 
 @Component({
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RecipeItemComponent implements OnInit {
+
+  @Input() element: {name: string, description: string, imagePath: string};
+
   constructor() { }
 
   ngOnInit() {
